42720d75711d8158e119d0c052879f5f2250b697
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-03-16 23:02:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// function scuberGreetingForFeet(Distance)
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  } 
 }
+console.log(scuberGreetingForFeet(3000));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// function ternaryCheckCity(destination)
+
+let destination;
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+console.log(ternaryCheckCity(destination));
+
+// function switchOnCharmFromTip()
+
+let tip;
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+console.log(switchOnCharmFromTip(tip));
